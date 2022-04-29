9bd7ce5b2b699e301a9b992e79176dace6a3529d
rauskov <uskov.ra@yandex.ru>
2022-04-29 12:52:38 +0800

    1-module 2-task

--- 1-module/2-task/index.js
+++ 1-module/2-task/index.js
@@ -2,7 +2,7 @@
  * Эту функцию трогать не нужно
  */
 function print(text) {
-  console.log(text);
+    console.log(text);
 }
 
 /**
@@ -10,15 +10,20 @@ function print(text) {
  * чтобы функция sayHello работала корректно
  */
 function isValid(name) {
-  // ваш код...
+    if (name === null) { return false } else if (name.indexOf(' ') >= 0 | name.length < 4) { return false } else { return true }
+
+
+
+
+    //if (name.indexOf(' ') < 0 & name.length >= 4 & name !== null) { return true } else { return false }
 }
 
 function sayHello() {
-  let userName = prompt('Введите ваше имя');
+    let userName = prompt('Введите ваше имя');
 
-  if (isValid(userName)) {
-    print(`Welcome back, ${userName}!`);
-  } else {
-    print('Некорректное имя');
-  }
+    if (isValid(userName)) {
+        print(`Welcome back, ${userName}!`);
+    } else {
+        print('Некорректное имя');
+    }
 }
