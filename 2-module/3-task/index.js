4a85cbc3d996ab63388aab115a56615e38522d13
rauskov <uskov.ra@yandex.ru>
2022-05-03 21:05:33 +0800

    *

--- 2-module/3-task/index.js
+++ 2-module/3-task/index.js
@@ -1,5 +1,17 @@
 let calculator = {
-  // ваш код
+    sum() {
+        return this.a + this.b
+    },
+
+    mul() {
+        return this.a * this.b
+    },
+
+    read(a, b) {
+        this.a = a;
+        this.b = b;
+    }
+
 };
 
 // НЕ УДАЛЯТЬ СТРОКУ, НУЖНА ДЛЯ ПРОВЕРКИ
