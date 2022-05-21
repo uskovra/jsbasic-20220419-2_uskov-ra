f6a39ce0b518503473920f1d18369fa0baef61c8
rauskov <uskov.ra@yandex.ru>
2022-05-21 21:27:17 +0800

    *

--- 5-module/3-task/index.js
+++ 5-module/3-task/index.js
@@ -4,8 +4,6 @@ function initCarousel() {
 
     let list = document.querySelector('.carousel__inner')
     let listElems = list.querySelectorAll('.carousel__slide')
-        //console.log(list, listElems)
-
 
     let width = list.offsetWidth // 988
     let position = 0
@@ -16,48 +14,26 @@ function initCarousel() {
 
     next.addEventListener('click', () => {
         count += 1
+
         if (count > 1) { prev.style.display = '' }
         if (count == 4) { next.style.display = 'none' }
-        position -= width
 
-        console.log(position)
+        position -= width
         position = Math.max(position, -width * (listElems.length - 1))
-        console.log(position)
+
         list.style.transform = `translateX(${position}px)`;
     })
 
     prev.addEventListener('click', () => {
         count -= 1
+
         if (count < 4) { next.style.display = '' }
         if (count == 1) { prev.style.display = 'none' }
+
         position += width
         position = Math.min(position, 0)
+
         list.style.transform = `translateX(${position}px)`;
     })
 
 }
-
-/*
-next.addEventListener('click', () => {
-  count += 1
-  if (count > 1) { prev.style.display = '' }
-  if (count == 4) { next.style.display = 'none' }
-  position -= width
-
-  console.log(position)
-  position = Math.max(position, -width * (listElems.length - 1))
-  console.log(position)
-      //list.style.marginLeft = position + 'px';
-  list.style.transform = `translateX(-${position}px)`;
-})
-
-prev.addEventListener('click', () => {
-  count -= 1
-  if (count < 4) { next.style.display = '' }
-  if (count == 1) { prev.style.display = 'none' }
-  position += width
-  position = Math.min(position, 0)
-      //list.style.marginLeft = position + 'px';
-  list.style.transform = `translateX(${position}px)`;
-})
-*/
